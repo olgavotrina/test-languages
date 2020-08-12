963d771c637ac004bc55585d46f4d3faf6bb9490
Olga Votrina <olga_votrina@mail.ru>
2020-08-12 21:22:49 +0700

    add tests for login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url.contains("login"), "URL doesn't contain 'login'"
+        current_url = self.browser.current_url
+        assert 'login' in current_url, "URL doesn't contain 'login'"
         assert True
 
     def should_be_login_form(self):
